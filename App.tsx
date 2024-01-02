ac11bc2cb17934ee188122541cd2b4be31b5a297
Vivek Mishra <vivekhere01@gmail.com>
2024-01-02 21:24:26 +0530

    add app icon

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, StatusBar} from 'react-native';
 import React from 'react';
 
 /**
@@ -16,31 +16,35 @@ const App = (): JSX.Element => {
   const onGenerateChallanHandler = (): void => {};
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <TouchableOpacity
-        onPress={onGenerateChallanHandler}
+    <>
+      <StatusBar backgroundColor="#1480c1" />
+      <View
         style={{
-          elevation: 5,
-          backgroundColor: '#1480c1',
-          borderRadius: 7,
-          paddingVertical: 15,
-          paddingHorizontal: 20,
+          flex: 1,
+          backgroundColor: 'white',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}>
-        <Text
+        <TouchableOpacity
+          onPress={onGenerateChallanHandler}
           style={{
-            color: 'white',
-            fontSize: 16,
-            fontWeight: 'bold',
+            elevation: 5,
+            backgroundColor: '#1480c1',
+            borderRadius: 7,
+            paddingVertical: 15,
+            paddingHorizontal: 20,
           }}>
-          Generate Challan
-        </Text>
-      </TouchableOpacity>
-    </View>
+          <Text
+            style={{
+              color: 'white',
+              fontSize: 16,
+              fontWeight: 'bold',
+            }}>
+            Generate Challan
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 };
 
